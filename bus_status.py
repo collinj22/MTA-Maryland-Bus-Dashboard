f1745091f5af087894e3531a3ef9c090277e08fd
collin <collinj22@gmail.com>
2017-04-01 16:05:42 -0400

    Initial Setup

--- bus_status.py
+++ bus_status.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
 # MTA Baltimore Bus Tracker
-import sys, getopt
+
 import pandas as pd
 import urllib.request
-import re
 from bs4 import BeautifulSoup
-import pyprind
+import gtfs_realtime_pb2
+import urllib
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = urllib.request.urlopen('http://gtfsrt.mta.maryland.gov:8888/TMGTFSRealTimeWebService/TripUpdate/TripUpdates.pb')
+feed.ParseFromString(response.read())
+for entity in feed.entity:
+  if entity.HasField('trip_update'):
+    print(entity.trip_update)
+
+
+def bus_time(stop):
+    html = urllib.request.urlopen(job_url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    texts = soup.findAll(text=True)
+    pass
